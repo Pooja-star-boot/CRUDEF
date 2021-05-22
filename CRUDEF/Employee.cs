fc72f3a8aca5794323a62a16a6d98029b24db263
Pooja Patel <pooja.b.patel@capgemini.com>
2021-05-23 03:55:54 +0530

    Add project files.

---
+++ CRUDEF/Employee.cs
@@ -0,0 +1,29 @@
+namespace CRUDEF
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Employee")]
+    public partial class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [StringLength(50)]
+        public string Address1 { get; set; }
+
+        [StringLength(50)]
+        public string Address2 { get; set; }
+
+        [StringLength(50)]
+        public string EmailID { get; set; }
+    }
+}
